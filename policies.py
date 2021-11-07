e7d1e955c550cb16ffc5755a3e8b3ef8b4892c3e
paoloburelli <burelli@gmail.com>
2021-11-07 10:54:06 +0100

    Updated policies to support new action mask

--- policies.py
+++ policies.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from stable_baselines3.ppo import PPO
 from models.cnnrl import CnnPPO
@@ -49,50 +51,34 @@ class Policies:
             self.budget = budget
 
         def predict(self, obs, state, deterministic):
-            action_masks = self.env.get_attr("action_mask")
+            valid_actions = self.env.get_attr("valid_action_list")
             actions = [self.__single_prediction(o, am, i, deterministic) for am, o, i in
-                       zip(action_masks, obs, range(len(action_masks)))]
+                       zip(valid_actions, obs, range(len(valid_actions)))]
             return actions, None
 
         def __score(self, obs, x, y, index):
             if self.simulate:
                 return self.env.env_method("simulate_click", (x, y), indices=index)[0]
             else:
-                return 1 + obs[x, y, 0] * (1 if obs[x, y, 1] > 0 else 0) + obs[x, y, 2]
+                return 0.1 + obs[x, y, 1]*100 + math.ceil(obs[x, y, 2])
 
-        def __single_prediction(self, obs, action_mask, index, deterministric):
-            budget_used = 0
+        def __single_prediction(self, obs, valid_actions_list, index, deterministric):
+            val = valid_actions_list
+            random.shuffle(val)
+            val = val[:min(self.budget, len(val))]
 
-            width = action_mask.shape[0]
-            height = action_mask.shape[1]
+            evaluated_moves = [{'move': a, 'score': pow(self.__score(obs, a[0], a[1], index), 2)} for a in val]
 
-            potentially_valid_moves = []
-            x_seq = list(range(width))
-            random.shuffle(x_seq)
+            if len(evaluated_moves) == 0:
+                return self.env.get_attr('action_space')[0].sample()
 
-            y_seq = list(range(height))
-            random.shuffle(y_seq)
-
-            for x in x_seq:
-                for y in y_seq:
-                    if action_mask[x, y] and (budget_used < self.budget or len(potentially_valid_moves) == 0):
-                        if budget_used >= self.budget:  # go over budget if no good solution is found yet
-                            logging.warning(
-                                f"{self.__class__.__name__}: estimation budget exceeded ({budget_used}/{self.budget})")
-                        score = self.__score(obs, x, y, index)
-                        budget_used += 1
-                        if score > 0:
-                            potentially_valid_moves.append({'move': (x, y), 'score': score * score})
-
-            if len(potentially_valid_moves) > 0:
-                if deterministric:
-                    return potentially_valid_moves[0]['move']
-                else:
-                    total_score = sum([c['score'] for c in potentially_valid_moves])
-                    probability = [c['score'] / total_score for c in potentially_valid_moves]
-                    return choice(potentially_valid_moves, p=probability)['move']
+            if deterministric:
+                evaluated_moves.sort(key=lambda m: m['score'])
+                return evaluated_moves[0]['move']
             else:
-                return random.randint(0, width - 1), random.randint(0, height - 1)
+                total_score = sum([c['score'] for c in evaluated_moves])
+                probability = [c['score'] / total_score for c in evaluated_moves]
+                return choice(evaluated_moves, p=probability)['move']
 
     class __RandomUniformPolicy:
         def __init__(self, env):
@@ -108,24 +94,9 @@ class Policies:
             self.env = env
             self.policy_name = "masked_random_uniform"
 
-        def __single_prediction(self, obs, action_mask):
-            width = action_mask.shape[0]
-            height = action_mask.shape[1]
-
-            potentially_valid_moves = []
-            for x in range(width):
-                for y in range(height):
-                    if action_mask[x, y]:
-                        potentially_valid_moves.append((x, y))
-
-            if len(potentially_valid_moves) > 0:
-                return random.sample(potentially_valid_moves, 1)[0]
-            else:
-                return random.randint(0, width - 1), random.randint(0, height - 1)
-
         def predict(self, obs, state, deterministic):
-            action_masks = self.env.get_attr("action_mask")
-            actions = [self.__single_prediction(o, am) for am, o in zip(action_masks, obs)]
+            valid_action_lists = self.env.get_attr("valid_action_list")
+            actions = [random.choice(val) for val in valid_action_lists]
             return actions, None
 
     @staticmethod
