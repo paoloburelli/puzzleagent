748e08cb9643ee65037b68803855b0fb122172be
paoloburelli <burelli@gmail.com>
2021-11-11 22:36:48 +0100

    Updated evaluation results

--- scripts/evaluate_single_level_cnn.sh
+++ scripts/evaluate_single_level_cnn.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-SIMS=6
-EPISODES=100
-MODEL_FILES=$(ls logs/test/ | grep CnnPPO_1)
+SIMS=4
+EPISODES=1000
+MODEL_FILES=$(ls logs/test/ | grep MaskableCnnPPO_)
+EVAL_FILE_NAME=trained_maskable_ppo
+FROM=101
 
 for MOD in $MODEL_FILES; do
-  scripts/run_sims.sh $SIMS
+  # scripts/run_sims.sh $SIMS
   LN=$(echo $MOD | sed 's/^[^_]*_//' | sed 's/_.*$//')
   TS=$(echo $MOD | sed 's/^.*\///')
-  python3 evaluate.py --seed 20142012 --episodes $EPISODES --n_envs $SIMS --train_session $TS trained_cnn_ppo $LN
+  if (($LN >= FROM)); then
+    python3 evaluate.py --seed 20142012 --episodes $EPISODES --n_envs $SIMS --train_session $TS $EVAL_FILE_NAME $LN
+  fi
 done
+
+cat logs/eval/${EVAL_FILE_NAME}_* > logs/eval/${EVAL_FILE_NAME}-merged.csv
+rm -f logs/eval/${EVAL_FILE_NAME}_*
